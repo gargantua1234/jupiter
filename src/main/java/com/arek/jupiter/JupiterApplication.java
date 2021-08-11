377ff217da97cf714182fe69a8f2ff0aa3077ab1
ablech <arek_b1993@poczta.fm>
2021-08-11 22:14:45 +0200

    h2 configuration in standalone property file (proper way)

--- src/main/java/com/arek/jupiter/JupiterApplication.java
+++ src/main/java/com/arek/jupiter/JupiterApplication.java
@@ -1,14 +1,11 @@
 package com.arek.jupiter;
 
-import com.arek.jupiter.configs.DBConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @PropertySource("classpath:db.properties")
-@EnableConfigurationProperties(DBConfigProperties.class)
 public class JupiterApplication {
 
     public static void main(String[] args) {
