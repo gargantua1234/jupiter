7ea991e352519bd3157cd9edfcfbe6325c284f1e
ablech <arek_b1993@poczta.fm>
2021-08-11 22:10:08 +0200

    h2 configuration using configurationProperties

--- src/main/java/com/arek/jupiter/JupiterApplication.java
+++ src/main/java/com/arek/jupiter/JupiterApplication.java
@@ -4,8 +4,10 @@ import com.arek.jupiter.configs.DBConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:db.properties")
 @EnableConfigurationProperties(DBConfigProperties.class)
 public class JupiterApplication {
 
--- src/main/java/com/arek/jupiter/configs/DBConfig.java
+++ src/main/java/com/arek/jupiter/configs/DBConfig.java
@@ -1,15 +1,14 @@
 package com.arek.jupiter.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import javax.sql.DataSource;
 
 @Configuration
-@PropertySource("classpath:credentials.properties")
 public class DBConfig {
 
     @Bean
