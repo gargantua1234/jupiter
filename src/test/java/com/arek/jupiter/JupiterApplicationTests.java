2ae32320b9d75770719c7161f572c86b88580c91
ablech <arek_b1993@poczta.fm>
2021-08-11 09:44:34 +0200

    initial commit

---
+++ src/test/java/com/arek/jupiter/JupiterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arek.jupiter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JupiterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
