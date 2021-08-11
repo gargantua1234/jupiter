573c48467eb5816ad5922e19c1b24030c9b2a63b
ablech <arek_b1993@poczta.fm>
2021-08-11 11:48:10 +0200

    adding main controller

---
+++ src/main/java/com/arek/jupiter/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.arek.jupiter.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class MainController {
+
+    @GetMapping("hello")
+    public String getHello(){
+        return "Hello World";
+    }
+}
