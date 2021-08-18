56317b11f69933a8416bdd9a6cc50dd60c332ea0
ablech <arek_b1993@poczta.fm>
2021-08-18 21:06:08 +0200

    added in memory userDetails beans

--- security/src/main/java/com/arek/security/config/AppSecurityConfig.java
+++ security/src/main/java/com/arek/security/config/AppSecurityConfig.java
@@ -1,13 +1,25 @@
 package com.arek.security.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@ComponentScan({"com.arek.security"})
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
@@ -18,5 +30,16 @@ public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        final UserDetails johnUser = User.builder()
+                .username("jon")
+                .password(passwordEncoder.encode("pass"))
+                .roles("STUDENT")
+                .build();
+        return new InMemoryUserDetailsManager(johnUser);
     }
 }
